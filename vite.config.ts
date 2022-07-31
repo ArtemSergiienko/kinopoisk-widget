17bc4d0718b3e9d4bcfbda8b625d3f6549e7fee1
ArtemSergiienko <55659143+ArtemSergiienko@users.noreply.github.com>
2022-07-31 20:49:45 +0300

    edit deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/kinopoisk-widget/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
