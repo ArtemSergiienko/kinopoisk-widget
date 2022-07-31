8cb60b57a1228bec09b0d263e5b75abe202c211d
ArtemSergiienko <55659143+ArtemSergiienko@users.noreply.github.com>
2022-07-31 14:33:15 +0300

    Adding dist

--- src/composables/useRequest.ts
+++ src/composables/useRequest.ts
@@ -1,11 +1,11 @@
 import { ref, reactive } from "vue";
 import axios from "axios";
 
-export const useFilms = (url: string, params: {}) => {
+export const useRequiest = (url: string, params: {}) => {
   let response = ref();
 
   const request = async () => {
-    const res = await axios.get(`https://kinopoiskapiunofficial.tech/api/v2.2/${url}`,{
+    const res = await axios.get(`https://kinopoiskapiunofficial.tech/api/${url}`,{
       headers: {
         // 'X-API-KEY': '604569b3-19a1-4526-a4ea-f1d61b46671b',
         'X-API-KEY': '193078cd-04ef-4964-a6a9-d0b6f3aaebc8',
